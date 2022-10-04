48466b62b124577c427027ce0abbe438330df565
Lucas Santos <lucas.santos.silva2011@gmail.com>
2022-10-04 20:03:41 -0300

    Added removeNode method

--- linked-lists/src/Main.java
+++ linked-lists/src/Main.java
@@ -11,38 +11,77 @@ public class Main {
         Node node1 = new Node(1);
         Node node2 = new Node(2);
         Node node3 = new Node(3);
+        Node node4 = new Node(4);
+        Node node5 = new Node(5);
+        Node node6 = new Node(6);
+        Node node7 = new Node(7);
 
         node1.setNext(node2);
         node2.setNext(node3);
+        node3.setNext(node4);
+        node4.setNext(node5);
+        node5.setNext(node6);
+        node6.setNext(node7);
 
-        // Percorrendo lista ligada
         traverse(node1);
 
-        // Adicionando um novo nó
-        Node novaLista = addNode(node1, 4);
-        System.out.println("Percorrendo lista adicionada");
-        traverse(novaLista);
+        Node newList = removeNode(node1, 7);
+        System.out.println("Lista com item removido");
+        traverse(newList);
+
     }
 
     public static void traverse(Node head) {
-        Node tempNode = head;
+        Node currentNode = head;
 
-        while(tempNode != null) {
-            System.out.println(tempNode.getValue());
-            tempNode = tempNode.getNext();
+        while(currentNode != null) {
+            System.out.println(currentNode.getValue());
+            currentNode = currentNode.getNext();
         }
     }
 
     public static Node addNode(Node head, int newValue) {
         Node newNode = new Node(newValue);
 
-        Node currentNode = head;
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node currentNode = head;
 
-        while (currentNode.hasNext()) {
-            currentNode = currentNode.getNext();
+            while (currentNode.hasNext()) {
+                currentNode = currentNode.getNext();
+            }
+
+            currentNode.setNext(newNode);
         }
 
-        currentNode.setNext(newNode);
+        return head;
+    }
+
+    public static Node removeNode(Node head, int value) {
+        if (head == null) {
+            System.out.println("Nada a ser removido");
+            return null;
+        }
+
+        Node currentNode = head;
+
+        if (currentNode.getValue() == value) {
+            return currentNode.getNext();
+        } else {
+            Node previousNode = currentNode;
+
+            while (currentNode != null) {
+                if (currentNode.getValue() == value) {
+                    previousNode.setNext(currentNode.getNext());
+                    return head;
+                } else {
+                    previousNode = currentNode;
+                    currentNode = currentNode.getNext();
+                }
+            }
+        }
+
         return head;
     }
 }
